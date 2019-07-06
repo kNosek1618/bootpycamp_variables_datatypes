0d3429f2912801e7083f5f72fd1536717804fa5e
root <kNosek1618>
2019-07-06 14:40:44 +0000

    information

---
+++ variables_datatypes.py
@@ -0,0 +1,29 @@
+#kind of variables name and using
+_x = 2
+print(_x)
+
+all, at, once = 5, 10, 15
+print(all)
+print(at)
+print(once)
+
+#python is the following change the variable
+x = "Peter"
+x = 15
+x = False
+print(x)
+
+#if use bool, u must use first upper letter for True and False
+is_active = True
+game_over  = False
+
+#None representing nothing
+#>>> None
+#result = nothing
+
+#list []
+table = [1,2,3,]
+
+#dict {}
+student = {'name': 'John', 'age': 25, 'courses': ['Math', "CompSci"]}
+print
